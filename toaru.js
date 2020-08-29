805adf5492326592797a3dafc63101f2352680f6
Rapha <61619471+raphasanori@users.noreply.github.com>
2020-08-29 19:16:20 -0300

    Update toaru.js

--- toaru.js
+++ toaru.js
@@ -2,3 +2,17 @@ var imagens = $(".corpopost a").filter(':has(img)');
 for (var i = 0; i < imagens.length; i++) {
     imagens[i].removeAttribute("href");
 }
+
+function mostra_disqus() {
+    var disqus_config = function() {
+        this.page.url = "<data:blog.canonicalUrl />";
+        this.page.identifier = "<data:post.id />";
+    };
+    (function() {
+        var d = document,
+            s = d.createElement('script');
+        s.src = 'https://toaru.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+}
